786db84f4d7a053c501750dda833bd5d536d83ff
Patrycjusz Mania <worms308@wp.pl>
2020-06-02 15:11:07 +0200

    Init Commit Added Person entity

---
+++ UsersApp/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp.Model
+{
+    class Person
+    {
+        private String firstName;
+        private String lastName;
+        private String streetName;
+        private String houseNumber;
+        private String apartmentNumber;
+    }
+}
